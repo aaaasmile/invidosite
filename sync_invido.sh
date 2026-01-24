c8281405f5f3708b0bd465b95fe773cc4f9ee1a4
aaaasmile <testhaus@gmx.at>
2026-01-24 20:58:17 +0100

    removed

--- sync_invido.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# aggiornare post, pages e tutta la parte statica compreso js:
-rsync -azv --delete /mnt/d/Projects/go-lang/invido-site/invido-site/static/invido/ igor@invido.it:/var/www/invido.it/html
-
